78ab6d58c5b70dadbe6a50e4843d268fa8c69e1b
j-osypiuk <kuba6535@gmail.com>
2023-05-05 15:33:06 +0200

    Basic ViewModel classes

---
+++ ToDoList.Core/ViewModels/Controls/UserTaskViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Core
+{
+	public class UserTaskViewModel
+	{
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Deadline { get; set; }
+    }
+}
---
+++ ToDoList.Core/ViewModels/Pages/UserTaskPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace ToDoList.Core
+{
+	public class UserTaskPageViewModel
+	{
+        public List<UserTaskViewModel> UserTaskList { get; set; } = new List<UserTaskViewModel>();
+
+        public string NewUserTaskTitle { get; set; }
+
+        public string NewUserTaskDescription { get; set; }
+
+        public DateTime NewUserTaskDeadline { get; set; }
+
+        public void AddNewTask()
+        {
+            var newUserTask = new UserTaskViewModel()
+            {
+                Title = NewUserTaskTitle,
+                Description = NewUserTaskDescription,
+                Deadline = NewUserTaskDeadline,
+            };
+
+            UserTaskList.Add(newUserTask);
+        }
+    }
+}
